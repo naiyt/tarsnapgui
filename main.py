d692fd478f190157f1cb76d91d028b99ba6645d5
Nate Collings <nate@natecollings.com>
2013-11-15 00:39:52 -0700

    Retrieve and display archives and basic stats

--- main.py
+++ main.py
@@ -3,97 +3,128 @@ import wx
 import threading
 import tarsnap as ts
 
+debugging = False
 
 class TarFrame(wx.Frame):
-	"""We simply derive a new class of Frame."""
-	def __init__(self, parent, title):
-		# Initialize things
-		wx.Frame.__init__(self, parent, title=title, size=(500,500))
-		self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
-		self.CreateStatusBar()
-		self.tarsnap = ts.TarSnap()
-		filemenu = wx.Menu()
+    """ Top level frame. Doesn't know about Tarsnap. """
+    def __init__(self, parent, title):
+        wx.Frame.__init__(self, parent, title=title, size=(500,500))
+        self.CreateStatusBar()
+        filemenu = wx.Menu()
 
-		# File options
-		menuAbout = filemenu.Append(wx.ID_ABOUT, "&About"," Information about this program")
-		menuExit = filemenu.Append(wx.ID_EXIT, "E&xit", " Terminate the program")
+        menuAbout = filemenu.Append(wx.ID_ABOUT, "&About"," Information about this program")
+        menuExit = filemenu.Append(wx.ID_EXIT, "E&xit", " Terminate the program")
 
-		# Tarsnap options
-		tarsnap_menu = wx.Menu()
-		list = tarsnap_menu.Append(wx.ID_ANY, "&List Archives", " List all your current tarsnap archives")
-		stats = tarsnap_menu.Append(wx.ID_ANY, "&Print Stats", "Print current stats")
-		stats_for = tarsnap_menu.Append(wx.ID_ANY, "&Print Stats for", "Print stats for a user")
+        # Create menu bar
+        menuBar = wx.MenuBar()
+        menuBar.Append(filemenu, "&File")
+        self.SetMenuBar(menuBar)
 
-		# Create menu bar
-		menuBar = wx.MenuBar()
-		menuBar.Append(filemenu, "&File")
-		menuBar.Append(tarsnap_menu, "&Tarsnap")
-		self.SetMenuBar(menuBar)
+        # File menu events
+        self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)
+        self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
 
-		# File menu events
-		self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)
-		self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
+        self.Show(True)
 
-		# Tarsnap menu events
-		self.Bind(wx.EVT_MENU, lambda event: self.TarsnapCommand(event, ts.LIST), list)
-		self.Bind(wx.EVT_MENU, lambda event: self.TarsnapCommand(event, ts.STATS), stats)
-		self.Bind(wx.EVT_MENU, lambda event: self.TarsnapCommand(event, ts.STATS,
-			archive="icarus-2013-11-11"), stats_for)
+    def OnAbout(self, e):
+        dlg = wx.MessageDialog(self, "A Tarsnap GUI", "About Tarsnap GUI", wx.OK)
+        dlg.ShowModal()
+        dlg.Destroy()
 
-		# Threading events
-		self.Bind(EVT_THREAD, self.OnFinishedCommand)
+    def OnExit(self, e):
+        self.Close(True)
 
-		self.Show(True)
 
-	def OnAbout(self, e):
-		dlg = wx.MessageDialog(self, "A Tarsnap GUI", "About Tarsnap GUI", wx.OK)
-		dlg.ShowModal()
-		dlg.Destroy()
+class TarWindow(wx.Panel):
+    """ The main panel. Most of the tarsnap interactino should happen here. """
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent)
+        self.parent = parent
+        self.tarsnap = ts.TarSnap(debugging)
 
-	def OnExit(self, e):
-		self.Close(True)
+        # Begin loading the tarsnap archive info when the app starts up
+        self.TarsnapCommand(None, ts.LIST)
+        self.TarsnapCommand(None, ts.STATS)
 
-	def TarsnapCommand(self, e, command, **kwargs):
-		"""Create a thread for a tarsnap command"""
-		worker = TarsnapThread(self, command, self.tarsnap, **kwargs)
-		worker.start()
+        grid = wx.GridBagSizer(hgap=5, vgap=5)
+        self.archive_label = wx.StaticText(self, label="Loading info on your current archives...")
+        self.stats_label = wx.StaticText(self, label="Loading stats for your current archives...")
+        grid.Add(self.archive_label, pos=(0,0))
+        grid.Add(self.stats_label, pos=(0,2))
 
-	def OnFinishedCommand(self, e):
-		"""
-		Run after a succesfull TarSnap command
-		TODO - check the eid for the command type, and handle it accordingly in the frame
-		Or maybe have them as different event thread types?
-		"""
-		self.control.SetValue(e.GetValue())
+        # Buttons and sizing
+        self.button_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.create_button = wx.Button(self, -1, "Create an Archive")
+        self.restore_button = wx.Button(self, -1, "Restore an Archive")
+        self.stats_button = wx.Button(self, -1, "Stats")
+        self.buttons = [self.create_button, self.restore_button, self.stats_button]
+        for button in self.buttons:
+            self.button_sizer.Add(button, wx.EXPAND)
+
+        self.sizer = wx.BoxSizer(wx.VERTICAL)
+        self.sizer.Add(self.button_sizer, 0, wx.EXPAND)
+        self.sizer.Add(grid, 0, wx.ALL, 5)
+
+        self.SetSizer(self.sizer)
+        self.SetAutoLayout(1)
+        self.sizer.Fit(self)
+
+        # Event bound to a returned command
+        self.Bind(EVT_THREAD, self.OnFinishedCommand)
+
+
+        self.Show(True)
+
+    def TarsnapCommand(self, e, command, **kwargs):
+        """Create a thread for a tarsnap command"""
+        worker = TarsnapThread(self, command, self.tarsnap, **kwargs)
+        worker.start()
+
+    def OnFinishedCommand(self, e):
+        """
+        Run after a succesfull TarSnap command
+        TODO - check the eid for the command type, and handle it accordingly in the frame
+        Or maybe have them as different event thread types?
+        """
+        result = e.GetValue()
+        if 'archive_list' in result:
+            archive_str = "Archives:\n"
+            archives = '\n'.join(result['archive_list'])
+            self.archive_label.SetLabel(archive_str + archives)
+        if 'stats' in result:
+            stats_str = "Stats:\n{}".format(result['stats'])
+            self.stats_label.SetLabel(stats_str)
 
 
 myEVT_THREAD = wx.NewEventType()
 EVT_THREAD = wx.PyEventBinder(myEVT_THREAD, 1)
 class ThreadedEvent(wx.PyCommandEvent):
-	""" Event to signal that a tarsnap command has finished """
-	def __init__(self, etype, eid, value=None):
-		wx.PyCommandEvent.__init__(self, etype, eid)
-		self.eid = eid
-		self._value = value
+    """ Event to signal that a tarsnap command has finished """
+    def __init__(self, etype, eid, value=None):
+        wx.PyCommandEvent.__init__(self, etype, eid)
+        self.eid = eid
+        self._value = value
 
-	def GetValue(self):
-		return self._value
+    def GetValue(self):
+        return self._value
 
 
 class TarsnapThread(threading.Thread):
-	def __init__(self, parent, command, tarsnap, **kwargs):
-		threading.Thread.__init__(self)
-		self.command = command
-		self.tarsnap = tarsnap
-		self.kwargs = kwargs
-		self._parent = parent
+    def __init__(self, parent, command, tarsnap, **kwargs):
+        threading.Thread.__init__(self)
+        self.command = command
+        self.tarsnap = tarsnap
+        self.kwargs = kwargs
+        self._parent = parent
 
-	def run(self):
-		self._value = self.tarsnap.run_command(self.command, **self.kwargs)
-		evt = ThreadedEvent(myEVT_THREAD, self.command, self._value) # Callback
-		wx.PostEvent(self._parent, evt)
+    def run(self):
+        self._value = self.tarsnap.run_command(self.command, **self.kwargs)
+        evt = ThreadedEvent(myEVT_THREAD, self.command, self._value) # Callback
+        wx.PostEvent(self._parent, evt)
 
 
 app = wx.App(False)
 frame = TarFrame(None, 'Tarsnap GUI')
+panel = TarWindow(frame)
+frame.Show()
 app.MainLoop()
--- tarsnap.py
+++ tarsnap.py
@@ -11,24 +11,51 @@ STATS = 5
 class NotImplemented(Exception):
 	pass
 
-class TarSnap():
-	"""docstring for TarSnap"""
+class TarBase():
+	def __init__(self):
+		pass
+
+	def _execute(self, command):
+		"""
+		Consolidating the actual execution here, in case we decide to use a different subprocess,
+		and for error handling/timeouts from TarSnap
+		"""
+
+		return check_output(command, shell=True) # TODO - Error handling here, timeouts, exceptions
+
+
+class TarSnap(TarBase):
+	"""
+	Basic TarSnap class. Basically just creates the stats object,
+	creates new archives, and lists current ones.
+
+	debug: if set to True, logs info and doesn't actually retrieve data from Tarsnap
+	(uses fake, testing data).
+	"""
 	def __init__(self, debug=True):
 		self.archives = None
 		self.stats = None
+		self.debug = debug
 
 	def run_command(self, command, **kwargs):
+
 		if command == LIST:
-			if self.archives is None:
-				self.archives = self._list()
+			# TODO - will eventually return actual archive objects
+			if self.debug:
+				self.archives = {"archive_list": ['archive1', 'archive2', 'archive4'] }
+			else:
+				if self.archives is None:
+					archives = self._list()
+					self.archives = {"archive_list": archives.split()}
 			return self.archives
 
 		elif command == STATS:
-			self.stats = self._stats(**kwargs)
+			# TODO - will eventually return a stats object
+			if self.debug:
+				self.stats = {"stats": "Using up all the bandwidth and space!"}
+			else:
+				self.stats = {"stats": self._stats(**kwargs)} 
 			return self.stats
-
-		elif command == EXCLUDE:
-			return self._exclude(**kwargs)
 
 		elif command == CREATE:
 			return self._create(**kwargs)
@@ -43,17 +70,21 @@ class TarSnap():
 			command = config.tarsnap_executable + " --print-stats -f " + kwargs['archive']
 		return self._execute(command)
 
-	def _exclude(self, **kwargs):
-		raise NotImplemented("Not yet implemented")
-
 	def _create(self, **kwargs):
 		raise NotImplemented("Not yet implemented")
 
 
-	def _execute(self, command):
-		"""
-		Consolidating the actual execution here, in case we decide to use a different subprocess,
-		and for error handling/timeouts from TarSnap
-		"""
+class Archive(TarBase):
+	""" Archives, that can be deleted, viewed, created, etc. """
+	def __init__(self):
+		pass
 
-		return check_output(command, shell=True) # TODO - Error handling here, timeouts, exceptions
+
+class Stats(TarBase):
+	"""
+	Class that can generate stats based on an entire account
+	or specific archives.
+	"""
+
+	def __init__(self):
+		pass		
